6e740dfc598b8eda9bc48205fc91d52abff42967
Allan James Lapid <ajLapid718@gmail.com>
2018-05-24 15:26:39 -0400

    add another action

--- 4-redux/src/js/client.js
+++ 4-redux/src/js/client.js
@@ -2,11 +2,15 @@ import { createStore } from 'redux';
 
 // Action Naming;
 const INC = "INC";
+const DEC = "DEC";
 
 // Reducer;
 const reducer = function(state, action) {
   if (action.type === INC) {
     return state + action.payload;
+  }
+  if (action.type === DEC) {
+    return state - action.payload;
   }
   return state;
 }
@@ -20,4 +24,8 @@ store.subscribe(() => {
 })
 
 // Dispatch;
+store.dispatch({type: INC, payload: 1});
+store.dispatch({type: INC, payload: 1});
+store.dispatch({type: DEC, payload: 1});
+store.dispatch({type: INC, payload: 1});
 store.dispatch({type: INC, payload: 1});
