e55797d8680d99d72b0ed6c7d3672a406d52b5a6
Allan James Lapid <ajLapid718@gmail.com>
2018-05-24 15:24:50 -0400

    skeleton setup

--- 4-redux/src/js/client.js
+++ 4-redux/src/js/client.js
@@ -1,13 +1,23 @@
 import { createStore } from 'redux';
 
-const reducer = function() {
-  return "foo";
+// Action Naming;
+const INC = "INC";
+
+// Reducer;
+const reducer = function(state, action) {
+  if (action.type === INC) {
+    return state + action.payload;
+  }
+  return state;
 }
 
+// Store;
 const store = createStore(reducer, 0);
 
+// Subscribe;
 store.subscribe(() => {
   console.log("store changed", store.getState());
 })
 
-store.dispatch({type: "INC", payload: 1});
+// Dispatch;
+store.dispatch({type: INC, payload: 1});
