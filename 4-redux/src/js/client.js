5ddd975b64b5e2ac9f8b9aadea86c65ff85ade1b
Allan James Lapid <ajLapid718@gmail.com>
2018-05-24 16:31:54 -0400

    import thunk from redux thunk

--- 4-redux/src/js/client.js
+++ 4-redux/src/js/client.js
@@ -1,15 +1,19 @@
 import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 const reducer = (state={}, action) => {
   return state;
 }
 
-const middleware = applyMiddleware(logger());
+const middleware = applyMiddleware(thunk, logger());
 const store = createStore(reducer, middleware);
 
 store.subscribe(() => {
   console.log("store changed", store.getState());
 })
 
-store.dispatch({type: "FOO"});
+store.dispatch((dispatch) => {
+  dispatch({type: "FOO"})
+  dispatch({type: "BAR"})
+}
