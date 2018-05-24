71db8ae0b61e88ddd39da18efe5bdb8e17b67d9f
Allan James Lapid <ajLapid718@gmail.com>
2018-05-24 16:45:38 -0400

    achieve the same logs except with a promise

--- 4-redux/src/js/client.js
+++ 4-redux/src/js/client.js
@@ -13,31 +13,25 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "FETCH_USERS_START":
+    case "FETCH_USERS_PENDING":
       return {...state, fetching: true};
-    case "FETCH_USERS_ERROR":
+    case "FETCH_USERS_REJECTED":
       return {...state, fetching: false, error: action.payload};
-    case "RECEIVE_USERS":
+    case "RECEIVE_USERS_FULFILLED":
       return {...state, fetching: false, fetched: true, users: action.payload};
     default:
       return state;
     }
 }
 
-const middleware = applyMiddleware(thunk, logger());
+const middleware = applyMiddleware(promise(), thunk, logger());
 const store = createStore(reducer, middleware);
 
 store.subscribe(() => {
   console.log("store changed", store.getState());
 })
 
-store.dispatch((dispatch) => {
-  dispatch({type: "FETCH_USERS_START"})
-  axios.get("http://rest.learncode.academy/api/wstern/users")
-    .then((response) => {
-      dispatch({type: "RECEIVE_USERS", payload: response.data})
-    })
-    .catch((err) => {
-      dispatch({type: "FETCH_USERS_ERROR", payload: err})
-    })
+store.dispatch({
+  type: "FETCH_USERS",
+  payload: axios.get("http://rest.learncode.academy/api/wstern/users")
 });
