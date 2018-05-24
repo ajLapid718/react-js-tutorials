435c8821ff71e17a1eb86eef124a4679bfbad6f1
Allan James Lapid <ajLapid718@gmail.com>
2018-05-24 16:28:50 -0400

    skeleton setup

---
+++ 4-redux/src/js/client.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, createStore } from "redux";
+
+const reducer = (state={}, action) => {
+  return state;
+}
+
+const middleware = applyMiddleware();
+const store = createStore(reducer, middleware);
+
+store.subscribe(() => {
+  console.log("store changed", store.getState());
+})
+
+store.dispatch({type: "FOO"});
